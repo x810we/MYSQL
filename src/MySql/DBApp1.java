e99952ba024fafbeeca06a34d88870c93d34f561
x810we <ingo.weyck@t-online.de>
2024-09-25 15:56:19 +0200

    Das erste Commit für MySql 22

--- src/MySql/DBApp1.java
+++ src/MySql/DBApp1.java
@@ -1,4 +1,4 @@
-
+package MySql;
 //Test
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/MySql/Test.java
+++ src/MySql/Test.java
@@ -1,6 +1,8 @@
+package MySql;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Test {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
